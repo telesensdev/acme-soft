c0fc7af1562c79d80f38eae07ae99f5f120ea9ae
telesensdev <bdmtelesens@gmail.com>
2020-06-10 17:22:41 +0300

    fix: clear trash

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,5 @@
 'use strict';
 
 $(document).ready(function () {
-    // Resize function
-    (function resizeFn() {
-        let doit;
 
-        const resized = () => {
-            // Call your functions
-        };
-
-        window.onresize = () => {
-            clearTimeout(doit);
-            doit = null;
-            doit = setTimeout(resized, 100);
-        };
-    })();
 });
